f88dc1acdadc80c41b63df7789e3531e30cc30cc
AkaneTan <69606737+AkaneTan@users.noreply.github.com>
2025-08-02 06:14:13 +0800

    build: Update dependencies
    Signed-off-by: AkaneTan <69606737+AkaneTan@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    val agpVersion = "8.10.0-rc01"
+    val agpVersion = "8.12.0"
     id("com.android.application") version agpVersion apply false
     id("com.android.library") version agpVersion apply false
-    kotlin("android") version "2.0.21" apply false
+    kotlin("android") version "2.2.0" apply false
 }
